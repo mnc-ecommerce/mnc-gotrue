49d87ef8167514723c2e89ff8066b6d8bf803a4f
beruang <zackymughnii@gmail.com>
2023-11-15 13:32:00 +0700

    Upgrade version of FlashMobile SMS OTP API to version 3.0.0

--- internal/api/sms_provider/sms_provider.go
+++ internal/api/sms_provider/sms_provider.go
@@ -41,6 +41,8 @@ func GetSmsProvider(config conf.GlobalConfiguration) (SmsProvider, error) {
 		return NewVonageProvider(config.Sms.Vonage)
 	case "flashmobile":
 		return NewFlashMobileProvider(config.Sms.FlashMobile)
+	case "flashmobilev3":
+		return NewFlashMobileV3Provider(config.Sms.FlashMobileV3)
 	default:
 		return nil, fmt.Errorf("sms Provider %s could not be found", name)
 	}
